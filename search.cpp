1d54f22279feea752305b1a19dae73cd8828a8e2
Pablo Emanuell Lopes Targino <pabloemanuell@A205-12L.imd.local>
2018-03-09 13:14:27 -0300

    Adicionado Jump search

--- search.cpp
+++ search.cpp
@@ -1,7 +1,7 @@
 #include <iostream>  // cout, endl
 #include <algorithm> // copy
 #include <iterator>  // ostream_iterator, begin(), end()
-
+#include <cmath>     // sqrt
 /// Implements an iterative linear seach on an range [first; last) of integers.
 /*!
  * \param first Pointer to the first element in the range.
@@ -11,7 +11,8 @@
  */
 
 using namespace std;
-int * lsearch(int *first, int *last, int value )
+
+int* lsearch(int *first, int *last, int value )
 {
     while(first != last){
         if(*first == value)
@@ -28,7 +29,7 @@ int * lsearch(int *first, int *last, int value )
  * \param value The target value we are looking for within the range.
  * \return A pointer to the target value within the range; or NULL, in case the value is not in the range.
  */
-int * bsearch(int *first, int *last, int value )
+int* bsearch(int *first, int *last, int value )
 {
     int m = distance(first, last);
     int *middle = first + (m/2);
@@ -53,20 +54,37 @@ int * bsearch(int *first, int *last, int value )
     return NULL; 
 }
 
+int* jsearch(int *first, int *last, int value )
+{
+    int n = last-first+1;
+    int m = sqrt(n);
+    int i;
+    int *e;
+    for(i = m; i < n; i+=m){
+        e = first+i;
+        if(value == *e)
+            return e;
+        else if(value < *e)
+            return lsearch(e-i, e, value);
+        else
+            continue;
+    }
+    return NULL;
+}
 
 // Driver function.
 int main()
 {
     int A[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Data container.
     int targets[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 , -4, 20 }; // Target values for testing.
-    int* (*f[]) (int*, int*, int) = {lsearch, bsearch};
+    int* (*f[]) (int*, int*, int) = {lsearch, bsearch, jsearch};
     // Prints out the original data container.
     cout << "Array: [ ";
     copy(begin(A), end(A), ostream_iterator<int>( cout , " " ) );
     cout << "]\n";
 
     // Executes several searchs in the data container.
-    for(int c = 0; c < 2; c++){
+    for(int c = 0; c < 3; c++){
         cout << "------------------------------\nExecutando algoritmo numero " << c << ".\n------------------------------\n";
         for( const auto & e : targets )
         {
